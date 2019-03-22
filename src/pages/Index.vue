fbcb64c9c9b16dadac907657e1d90c51f648b78f
Brandon <tallain@gmail.com>
2019-03-22 10:57:49 -0700

    removed 'query' card from index for now

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,16 @@
 <template>
-  <b-container>
-    <h2 class="m-3">Welcome back, {{ this.$auth.user.given_name }}</h2>
+  <b-container class="col-sm-12 col-lg-6">
+    <h2 class="m-3">Welcome back, {{ this.$auth.user.given_name }}!</h2>
     <b-row>
-      <b-col sm="12" md="4">
-        <b-card
-          title="Words Written Today">
+      <b-col sm="6" class="mt-2">
+        <b-card title="Words Written Today">
           <b-card-text><h2>0</h2></b-card-text>
           <b-card-text><span class="text-muted">Goal: </span>{{ this.goal }}</b-card-text>
           <b-button variant="primary" size="sm" v-b-modal.update-goal>Add Words</b-button>
           <b-button variant="info" size="sm" v-b-modal.work-goal>Change Goal</b-button>
         </b-card>
       </b-col>
-      <b-col sm="12" md="4">
+      <b-col sm="6" class="mt-2">
         <b-card title="Last Updated Project">
           <b-card-text><h2>1Q84</h2></b-card-text>
           <b-card-text><span class="text-muted">Type: </span>Novel</b-card-text>
@@ -19,14 +18,14 @@
           <b-button variant="success" size="sm" to="/new-project">Start New Project</b-button>
         </b-card>
       </b-col>
-      <b-col sm="12" md="4">
+      <!-- <b-col sm="6" class="mt-2">
         <b-card title="Latest Query">
           <b-card-text><h2>Agent pitch</h2></b-card-text>
           <b-card-text><span class="text-muted">Last update: </span>2019/02/15</b-card-text>
           <b-button variant="primary" size="sm">View Details</b-button>
           <b-button variant="success" size="sm">Open New Query</b-button>
         </b-card>
-      </b-col>
+      </b-col> -->
     </b-row>
     <update-goal project-name="sdgf" />
     <work-goal />
