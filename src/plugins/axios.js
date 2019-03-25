529c8e2ee732dc07e96e569cb2f1047dd5c61ac4
Brandon <tallain@gmail.com>
2019-03-25 10:13:34 -0700

    changed axios plugin to be responsive to access token changes

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,13 +1,30 @@
 import axios from 'axios'
 
-let AUTH_TOKEN = 'Bearer ' + localStorage.getItem('access_token')
+const http = axios.create({
+  baseURL: 'http://127.0.0.1:5000/api/v1/resources',
+  timeout: 1000,
+  headers: {'Content-Type': 'application/json'}
+})
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000/api/v1/resources'
-axios.defaults.headers['Content-Type'] = 'application/json'
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
+http.interceptors.request.use(
+  function (config) {
+    const token = getBearerToken()
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+function getBearerToken () {
+  return localStorage.getItem('access_token')
+}
 
 export default {
   install: function (Vue) {
-    Vue.prototype.$axios = axios
+    Vue.prototype.$axios = http
   }
 }
