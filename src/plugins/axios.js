a236d7b70b3071804d32694eb13f3c4d20f38837
Brandon Gandy <tallain@gmail.com>
2019-04-02 21:50:39 -0700

    removed prepended data from base url for Axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/v1/resources',
+  baseURL: 'http://127.0.0.1:5000',
   timeout: 1000,
   headers: {'Content-Type': 'application/json'}
 })
