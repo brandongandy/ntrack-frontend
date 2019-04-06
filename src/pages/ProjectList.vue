77d9d85818a3b0c89fd62741bc46c0baa9eda57e
Brandon Gandy <tallain@gmail.com>
2019-04-06 11:36:56 -0700

    started refining design on ProjectList page

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,14 +1,40 @@
 <template>
-  <v-container grid-list-md>
+  <v-container fluid grid-list-md>
     <v-layout row wrap>
-      <v-flex xs12>
-        <h2 class="headline">My Active Projects</h2>
-      </v-flex>
-      <v-flex xs8><v-btn depressed small icon color="success"><v-icon>add</v-icon></v-btn></v-flex>
-    </v-layout>
-    <v-layout row wrap>
-      <v-flex xs8 v-for="project in projectList" :key="project.id">
-        <project-summary :project="project" :inList="true" />
+      <v-flex xs12 lg8>
+        <v-card class="mt-4">
+          <v-card-title>
+            <v-container class="primary white--text card-header elevation-1">
+              <v-layout align-center>
+                <v-flex>
+                  <span class="title">My Active Projects</span><v-spacer></v-spacer>
+                </v-flex>
+                <v-flex shrink class="pa-0">
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on }">
+                      <v-btn icon small depressed class="ma-0"
+                        color="accent"
+                        v-on="on"
+                        to="/project/new">
+                        <v-icon>add</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Add New Project</span>
+                  </v-tooltip>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-title>
+
+          <v-card-text>
+            <v-flex xs8></v-flex>
+            <v-layout row wrap>
+              <v-flex xs12 v-for="project in projectList" :key="project.id">
+                <project-summary :project="project" :inList="true" />
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
