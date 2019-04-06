2e6b56c468df85d7257ce5804b9202a4d84726d8
Brandon Gandy <tallain@gmail.com>
2019-04-06 10:59:59 -0700

    added entry to store index

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import project from './project'
+import entry from './entry'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
+    entry,
     project
   }
 })
